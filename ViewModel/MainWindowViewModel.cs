398cd23b7de8af7e4c11a29366ecdc2dc45b2e0b
Hikager <lluis22.ml@gmail.com>
2021-01-04 14:20:36 +0100

    What:
    1-Create new foler ViewModel (for the mvvm pattern)
    2-Add a new class
    3-bind the new class into MainWindow.xaml  (data context binding).
    
    Why:
    
    We need a view-model path to store the logic.
    We  need bind one class (view-model) to its view if we want any logic on the window.

---
+++ ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace m6.uf4.dialogs.proba02.git.ViewModel
+{
+    public class MainWindowViewModel
+    {
+    }
+}
