182f8822c89d78e26a2aa2c6a37bba6377461412
Hikager <lluis22.ml@gmail.com>
2021-01-04 14:10:02 +0100

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace m6.uf4.dialogs.proba02.git
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
