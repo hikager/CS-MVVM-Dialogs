e6db596460f9115a0e30de65a41fb7569ea3ecfe
Hikager <lluis22.ml@gmail.com>
2021-01-04 14:34:49 +0100

    Model view logic preparation - create properties to be bound with view

--- ViewModel/MainWindowViewModel.cs
+++ ViewModel/MainWindowViewModel.cs
@@ -1,16 +1,36 @@
-﻿using System;
+﻿using GalaSoft.MvvmLight.Command;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace m6.uf4.dialogs.proba02.git.ViewModel
 {
     public class MainWindowViewModel : INotifyPropertyChanged
     {
 
+        #region properties for view
+
+        private string _text;
+        public string Text
+        {
+            get { return _text; }
+            set { _text = value; NotifyPropertyChanged(); }
+        }
+
+        #endregion
+
+
+        #region commands for view buttons logic
+
+        public ICommand BtnCommand => new RelayCommand<string>(NewDialog);
+        public void NewDialog(string isModal) { }
+
+        #endregion
 
 
         #region notify
@@ -23,6 +43,7 @@ namespace m6.uf4.dialogs.proba02.git.ViewModel
                 PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
             }
         }
+
         #endregion
     }
 }
